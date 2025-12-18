77f354ffe201f0a5b94230113053970ecc60a46b
chanpller <2856838704@qq.com>
2025-12-18 10:48:27 +0800

    first commit

---
+++ chapter-1-logging/src/main/java/com/chanpller/chapter1logging/Chapter1LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.chanpller.chapter1logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter1LoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter1LoggingApplication.class, args);
+    }
+
+}
---
+++ chapter-1-logging/src/test/java/com/chanpller/chapter1logging/Chapter1LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanpller.chapter1logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter1LoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
