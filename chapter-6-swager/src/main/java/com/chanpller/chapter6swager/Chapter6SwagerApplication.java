7799b705b7edfdbd0725f785dfb6c265d574b159
chanpller <2856838704@qq.com>
2026-03-12 22:43:29 +0800

    springboot3-场景整合-openapi

---
+++ chapter-6-swager/src/main/java/com/chanpller/chapter6swager/Chapter6SwagerApplication.java
@@ -0,0 +1,13 @@
+package com.chanpller.chapter6swager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter6SwagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter6SwagerApplication.class, args);
+    }
+
+}
---
+++ chapter-6-swager/src/test/java/com/chanpller/chapter6swager/Chapter6SwagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanpller.chapter6swager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter6SwagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
